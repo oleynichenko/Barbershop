4f48665a6cc903fcf3587b8c618a340684a8b1c5
Alex Oleynichenko <oleynichenkos@gmail.com>
2017-06-18 17:48:36 +0300

    Ready assembly

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,13 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -34,6 +40,54 @@ gulp.task("serve", ["style"], function() {
   gulp.watch("sass/**/*.scss", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
+
+
+// сборка
+
+gulp.task("style-build", function() {
+  gulp.src("build/css/style.css")
+    .pipe(postcss([
+      mqpacker({
+        sort: true
+        })
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("images", function() {
+  gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+       imagemin.optipng({optimizationLevel: 3}),
+       imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function() {
+ return gulp.src([
+   "fonts/**/*.{woff,woff2}",
+   "img/**",
+   "js/**",
+   "css/**",
+   "*.html"
+   ], {
+   base: "."
+   })
+ .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style-build", "images", fn);
+});
+
+
 
 
 
