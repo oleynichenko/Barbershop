5177f4a7929567160bac286d885fcb4f4bb93438
Alex Oleynichenko <oleynichenkos@gmail.com>
2017-06-19 11:20:02 +0300

    edits images paths

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,10 @@ var imagemin = require("gulp-imagemin");
 var run = require("run-sequence");
 var del = require("del");
 
+var realFavicon = require ('gulp-real-favicon');
+var fs = require('fs');
+
+
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
     .pipe(plumber())
@@ -24,6 +28,9 @@ gulp.task("style", function() {
         "last 2 Edge versions" 
       ]})
     ]))
+    .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
@@ -79,15 +86,127 @@ gulp.task("copy", function() {
  .pipe(gulp.dest("build"));
 });
 
+
 gulp.task("clean", function() {
   return del("build");
 });
 
 gulp.task("build", function(fn) {
-  run("clean", "copy", "style-build", "images", fn);
+  run("clean", "copy", "style-build", "images", "generate-favicon", "inject-favicon-markups", fn);
 });
 
+// Копирование для Портфолио
 
+gulp.task("copy-portfolio", function() {
+ return gulp.src([
+   "build/**/*.*"
+   ])
+ .pipe(gulp.dest("../oleynichenko.github.io/barbershop"));
+});
+
+gulp.task("clean-portfolio", function() {
+  return del(["../oleynichenko.github.io/barbershop/**", "!../oleynichenko.github.io/barbershop"], {force: true});
+});
+
+gulp.task("build-portfolio", function(fn) {
+  run("clean-portfolio", "copy-portfolio", fn);
+});
+
+//favicon
+
+// File where the favicon markups are stored
+var FAVICON_DATA_FILE = 'faviconData.json';
+
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', function(done) {
+  realFavicon.generateFavicon({
+    masterPicture: 'build/img/brb.png',
+    dest: 'build/img/icons',
+    iconsPath: 'img/icons/',
+    design: {
+      ios: {
+        pictureAspect: 'backgroundAndMargin',
+        backgroundColor: '#ffffff',
+        margin: '11%',
+        assets: {
+          ios6AndPriorIcons: false,
+          ios7AndLaterIcons: false,
+          precomposedIcons: false,
+          declareOnlyDefaultIcon: true
+        },
+        appName: 'BarberShop'
+      },
+      desktopBrowser: {},
+      windows: {
+        pictureAspect: 'noChange',
+        backgroundColor: '#e6e6e6',
+        onConflict: 'override',
+        assets: {
+          windows80Ie10Tile: false,
+          windows10Ie11EdgeTiles: {
+            small: false,
+            medium: true,
+            big: false,
+            rectangle: false
+          }
+        },
+        appName: 'BarberShop'
+      },
+      androidChrome: {
+        pictureAspect: 'noChange',
+        themeColor: '#f7f7f7',
+        manifest: {
+          name: 'BarberShop',
+          display: 'standalone',
+          orientation: 'notSet',
+          onConflict: 'override',
+          declared: true
+        },
+        assets: {
+          legacyIcon: false,
+          lowResolutionIcons: false
+        }
+      },
+      safariPinnedTab: {
+        pictureAspect: 'silhouette',
+        themeColor: '#4a4a4a'
+      }
+    },
+    settings: {
+      compression: 5,
+      scalingAlgorithm: 'Mitchell',
+      errorOnImageTooSmall: false
+    },
+    markupFile: FAVICON_DATA_FILE
+  }, function() {
+    done();
+  });
+});
+
+// Inject the favicon markups in your HTML pages. You should run
+// this task whenever you modify a page. You can keep this task
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', function() {
+  return gulp.src(['build/*.html'])
+    .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+    .pipe(gulp.dest('build'));
+});
+
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your
+// continuous integration system.
+gulp.task('check-for-favicon-update', function(done) {
+  var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+  realFavicon.checkForUpdates(currentVersion, function(err) {
+    if (err) {
+      throw err;
+    }
+  });
+});
 
 
 
